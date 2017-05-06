021b3bc337ec3f3f5d489e027995f40d4c971c28
Mickens <mauricemickens@Maurices-MacBook-Pro-2.local>
2017-05-06 19:07:16 -0400

    Stubbed out methods to start and reset game. Testing selecting a cell.

---
+++ Tic-Tac-Toe_Mickens/Tic-Tac-Toe_Mickens/Model.swift
@@ -0,0 +1,14 @@
+//
+//  Model.swift
+//  Tic-Tac-Toe_Mickens
+//
+//  Created by Maurice Mickens on 5/6/17.
+//
+//
+
+import Foundation
+
+enum Player{
+    case Human
+    case Computer
+}
--- Tic-Tac-Toe_Mickens/Tic-Tac-Toe_Mickens/ViewController.swift
+++ Tic-Tac-Toe_Mickens/Tic-Tac-Toe_Mickens/ViewController.swift
@@ -15,11 +15,47 @@ class BoardCell:UICollectionViewCell{
 class ViewController: UIViewController, CustomCollectionVC {
     
     @IBOutlet weak var collectionView:UICollectionView!
+    var humanMove = false
+    var computerMove = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
         configCollectionView(delegate: self)
         configureCollectionViewItemSize(collectionView: collectionView, layout: collectionView.collectionViewLayout as! UICollectionViewFlowLayout)
+    }
+    
+    @IBAction func computerStart(_ sender: Any) {
+        startNewGame(playerMove: Player.Computer)
+    }
+    
+    @IBAction func humanStart(_ sender: Any) {
+        startNewGame(playerMove: Player.Human)
+    }
+    
+    func startNewGame(playerMove:Player){
+        resetGame()
+        if playerMove == .Computer{
+            computerMove = true
+            humanMove = false
+            makeMove()
+        } else{
+            computerMove = false
+            humanMove = true
+            notifyTurnToMove()
+        }
+    }
+    
+    
+    func resetGame(){
+        
+    }
+    
+    func makeMove(){
+        
+    }
+    
+    func notifyTurnToMove(){
+        
     }
 }
 
@@ -41,6 +77,11 @@ extension ViewController: UICollectionViewDataSource{
 
 
 extension ViewController: UICollectionViewDelegate{
-    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        guard let boardCell = collectionView.cellForItem(at: indexPath) as? BoardCell else { return }
+        boardCell.backgroundColor = UIColor.green
+        
+    }
 }
 
