bfc3e0999d87483a93a465da96bacdb42b65dff7
Dave Haupert <dhaupert@ddhsoftware.com>
2017-10-10 09:59:16 -0400

    Initial Project created

---
+++ Tic-Tac-Toe Mobile/Tic-Tac-Toe Mobile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tic-Tac-Toe Mobile
+//
+//  Created by Dave Haupert on 10/10/17.
+//  Copyright © 2017 DDH Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view, typically from a nib.
+   }
+
+   override func didReceiveMemoryWarning() {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+   }
+
+
+}
+
