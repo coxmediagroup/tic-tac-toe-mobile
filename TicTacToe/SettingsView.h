2e68a27a5e11a640f64d5684da8fabf809ec9dab
trippinsoft <alan@trippinsoft.com>
2017-10-11 09:45:13 -0400

    Finished initial AI implementation based on miniMax algorithm Added settings view Added various images

---
+++ TicTacToe/SettingsView.h
@@ -0,0 +1,16 @@
+//
+//  SettingsView.h
+//  TicTacToe
+//
+//  Created by Alan on 10/10/17.
+//  Copyright © 2017 CMG. All rights reserved.
+//
+#import <UIKit/UIKit.h>
+
+@interface SettingsView : UIView
+
+-(id)initWithFrame:(CGRect)frame;
+
+@property (nonatomic, strong) NSString *letterSelection;
+
+@end
--- TicTacToe/ViewController.m
+++ TicTacToe/ViewController.m
@@ -5,18 +5,22 @@
 //  Created by Alan on 10/9/17.
 //  Copyright © 2017 CMG. All rights reserved.
 //
+//  Note:  I could have created this view using Interface Builder, but for this project I chose to do it all programmatically.  Either option is fine with me.
 
 #import "ViewController.h"
+#import "SettingsView.h"
 
 @interface ViewController ()
 
-@property (nonatomic, assign) int gameState;
+@property (assign, nonatomic) int gameState;
 @property (strong, nonatomic) NSString *humanPlayer;
 @property (strong, nonatomic) NSString *aiPlayer;
 @property (strong, nonatomic) NSString *currentPlayer;
 @property (strong, nonatomic) NSMutableArray *board;
 @property (assign, nonatomic) int bestScore;
 @property (assign, nonatomic) int bestMove;
+@property (strong, nonatomic) SettingsView *settings;
+@property (strong, nonatomic) NSMutableArray *selectionButtonsArray;
 
 @end
 
@@ -24,7 +28,17 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.selectionButtonsArray = [[NSMutableArray alloc] init];
     
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(startGame)
+                                                 name:@"startGame"
+                                               object:nil];
+
+    [self preparePlayBoard];
+    
+    [self displaySettings];
+    /*
     self.currentPlayer = @"X";
 
     self.aiPlayer = @"O";
@@ -35,9 +49,77 @@
     
     self.bestMove = [self findBestMove];
     NSLog(@"Best Move:%d",self.bestMove);
+     */
 }
 
--(int)findBestMove
+- (void)startGame
+{
+    // Display user letter selections at top
+}
+
+- (void)displaySettings
+{
+    // Initialize the window
+    int width = 300;
+    int height = 300;
+    CGRect frame = CGRectMake([UIScreen mainScreen].bounds.size.width/2-width/2, [UIScreen mainScreen].bounds.size.height+height, width, height);
+    self.settings = [[SettingsView alloc] initWithFrame:frame];
+    [self.view addSubview:self.settings];
+    
+    // Animate the window from the bottom
+    [UIView animateWithDuration:0.5f animations:^{
+        self.settings.frame = CGRectMake([UIScreen mainScreen].bounds.size.width/2-width/2, [UIScreen mainScreen].bounds.size.height/2-height/2-10, width, height);
+    } completion:^(BOOL finished) {
+        [UIView animateWithDuration:0.25f animations:^{
+            self.settings.frame = CGRectMake([UIScreen mainScreen].bounds.size.width/2-width/2, [UIScreen mainScreen].bounds.size.height/2-height/2+10, width, height);
+        } completion:^(BOOL finished) {
+        }];
+    }];
+}
+
+- (void)preparePlayBoard
+{
+    // Add the board background
+    UIImage *backgroundImage = [UIImage imageNamed:@"background"];
+    UIImageView *backgroundImageView = [[UIImageView alloc] init];
+    CGRect backgroundFrame = CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height);
+    backgroundImageView.frame = backgroundFrame;
+    [backgroundImageView setImage:backgroundImage];
+    [self.view addSubview:backgroundImageView];
+
+    // Add the background image
+    UIImage *boardBackgroundImage = [UIImage imageNamed:@"boardBackground"];
+    UIImageView *imageView = [[UIImageView alloc] init];
+    CGRect frame = CGRectMake([UIScreen mainScreen].bounds.size.width/2-150, [UIScreen mainScreen].bounds.size.height/2-140, 300, 300);
+    imageView.frame = frame;
+    [imageView setImage:boardBackgroundImage];
+    [self.view addSubview:imageView];
+
+    // Add play buttons
+    for (int i=0; i<9; i++) {
+        [self addButton:self.selectionButtonsArray selector:@selector(selectCategoryButton:) x:100 y:100 title:@"X" tag:i];
+    }
+}
+
+- (void)addButton:(NSMutableArray *)buttonArray
+         selector:(SEL)selector
+                x:(int)x
+                y:(int)y
+            title:(NSString *)title
+              tag:(int)tag
+{
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+    [button addTarget:self action:selector forControlEvents:UIControlEventTouchUpInside];
+    [button setTitle:title forState:UIControlStateNormal];
+    button.titleLabel.font = [UIFont fontWithName:@"Arial-BoldMT" size:16];
+    button.frame = CGRectMake(x, y, 50.0, 50.0);
+    [button setBackgroundColor:[UIColor darkGrayColor]];
+    [self.view addSubview:button];
+    [buttonArray addObject:button];
+    button.tag = tag;
+}
+
+- (int)findBestMove
 {
     int bestScore = -10000;
     NSArray *emptySpaces = [self getEmptySpaces:self.board];
@@ -62,7 +144,7 @@
     return self.bestMove;
 }
 
--(int)miniMax:(NSMutableArray *)board maximizingPlayer:(BOOL)maximizingPlayer
+- (int)miniMax:(NSMutableArray *)board maximizingPlayer:(BOOL)maximizingPlayer
 {
     // Using MiniMax algorithm
     
@@ -123,7 +205,7 @@
     return score;
 }
 
--(NSArray *)getEmptySpaces:(NSArray *)board
+- (NSArray *)getEmptySpaces:(NSArray *)board
 {
     NSMutableArray *emptySpaces = [[NSMutableArray alloc] init];
     int cnt = 0;
@@ -136,7 +218,7 @@
     return (NSArray *)emptySpaces;
 }
 
--(BOOL)winningMove:(NSArray *)board value:(NSString *)value
+- (BOOL)winningMove:(NSArray *)board value:(NSString *)value
 {
     if (([board[0] isEqualToString:value]) && ([board[1] isEqualToString:value]) && ([board[2] isEqualToString:value])) return YES;
     else if (([board[3] isEqualToString:value]) && ([board[4] isEqualToString:value]) && ([board[5] isEqualToString:value])) return YES;
@@ -149,6 +231,7 @@
     
     return NO;
 }
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     
